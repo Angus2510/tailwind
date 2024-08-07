836bb0fcbd0d31a120b213089e5faf5456413d15
Angus2510 <124569463+Angus2510@users.noreply.github.com>
2024-08-07 16:19:08 +0200

    second commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
+const plugin = require("tailwindcss");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        wavey: {
+          "0%, 100%": {
+            transform: "scaleY(0.5)",
+          },
+          "50%": {
+            transform: "scaleY(1.5)",
+          },
+        },
+      },
+      animation: {
+        wavey: "wavey 1s linear infinite",
+      },
+    },
   },
-  plugins: [require("./plugins/openVariant.js")],
+  plugins: [
+    require("./plugins/openVariant.js"),
+    require("./plugins/animationDelay.js"),
+  ],
 };
